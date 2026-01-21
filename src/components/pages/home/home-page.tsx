583044bdfe337752f904ef2fde7bed51a19d5c0a
Ivan Belyaev <i.beliaev@friendlee.ru>
2026-01-21 16:19:33 +0700

    test: check telegram hooks data

--- src/components/pages/home/home-page.tsx
+++ src/components/pages/home/home-page.tsx
@@ -1,10 +1,13 @@
 import { FinalSection } from '@/components/sections/final'
 import { GameInfo } from '@/components/shared/game-info'
+import { useLaunchParams, useRawInitData } from '@tma.js/sdk-react'
 import { useCurrentSection } from '../../../store/useCurrentSection'
 import { CardsContent } from '../../sections/CardsContent/CardsContent'
 import styles from './home-page.module.scss'
 
 export const HomePage = () => {
+	const initData = useRawInitData()
+	const launchParams = useLaunchParams()
 	const setCurrentSection = useCurrentSection(state => state.setCurrentSection)
 	const currentSection = useCurrentSection(state => state.currentSection)
 	if (currentSection === 'final') {
@@ -13,6 +16,12 @@ export const HomePage = () => {
 
 	return (
 		<main className={styles.main}>
+			<h1>{initData}</h1>
+			<h2>
+				{launchParams.tgWebAppData?.user?.first_name}{' '}
+				{launchParams.tgWebAppData?.user?.last_name}{' '}
+				{launchParams.tgWebAppData?.user?.username}
+			</h2>
 			<CardsContent
 				showNextSection={() => setCurrentSection('final')}
 				isAnimationStarted
